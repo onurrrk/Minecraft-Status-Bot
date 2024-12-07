ea93cccec3e916d7ca81c93a20489e8788cf22c1
onurr_k <145716470+onurrrk@users.noreply.github.com>
2024-12-07 11:22:18 +0300

    Add files via upload

--- src/events/messageCreate/auto-reply.js
+++ src/events/messageCreate/auto-reply.js
@@ -1,1 +1,44 @@
+const { autoReply, mcserver, commands, settings } = require('../../../config')
+const fs = require('fs')
+const json5 = require('json5')
 
+const languageAutoReply = settings.language.autoReply ? settings.language.autoReply : settings.language.main
+const fileContents = fs.readFileSync(`./translation/${languageAutoReply}/auto-reply.json5`, 'utf8')
+const autoReplyReplyText = json5.parse(fileContents)
+
+module.exports = async (msg) => {
+  try {
+    if (!autoReply.enabled || msg.author.bot || msg.content.startsWith(commands.prefixCommands.prefix)) return
+    const { content } = msg
+    const { ip, site, status, version } = autoReply
+    const isIp = new RegExp(`(?<=^|\\P{L})(${ip.triggerWords.join('|')})(?=\\P{L}|$)`, 'iu')
+    const isSite = new RegExp(`(?<=^|\\P{L})(${site.triggerWords.join('|')})(?=\\P{L}|$)`, 'iu')
+    const isStatus = new RegExp(`(?<=^|\\P{L})(${status.triggerWords.join('|')})(?=\\P{L}|$)`, 'iu')
+    const isVersion = new RegExp(`(?<=^|\\P{L})(${version.triggerWords.join('|')})(?=\\P{L}|$)`, 'iu')
+
+    if (isIp.test(content) && autoReply.ip.enabled) {
+      msg.reply(autoReplyReplyText.ip.replyText.replace(/{ip}/g, mcserver.ip).replace(/{port}/g, mcserver.port))
+    }
+    if (isSite.test(content) && autoReply.site.enabled && mcserver.site) {
+      msg.reply(autoReplyReplyText.site.replyText.replace(/{site}/g, mcserver.site))
+    }
+    if (isVersion.test(content) && autoReply.version.enabled) {
+      msg.reply(autoReplyReplyText.version.replyText.replace(/{version}/g, mcserver.version))
+    }
+    if (isStatus.test(content) && autoReply.status.enabled) {
+      await msg.channel.sendTyping()
+      const { getServerDataAndPlayerList } = require('../../index')
+      const { data, isOnline } = await getServerDataAndPlayerList(true)
+      msg.reply(
+        isOnline
+          ? autoReplyReplyText.status.onlineReply
+              .replace(/{playerOnline}/g, data.players.online)
+              .replace(/{playerMax}/g, data.players.max)
+          : autoReplyReplyText.status.offlineReply
+      )
+    }
+  } catch (error) {
+    const { getError } = require('../../index')
+    getError(error, 'autoReply')
+  }
+}
---
+++ src/events/messageCreate/prefixCommands.js
@@ -0,0 +1,62 @@
+const { commands, mcserver } = require('../../../config')
+const { cmdSlashTranslation } = require('./../../index')
+const { ipEmbed, siteEmbed, playerList, versionEmbed, statusEmbed, motdEmbed, helpEmbed } = require('../../embeds')
+
+module.exports = async (message, client) => {
+  if (
+    message.author.bot ||
+    !commands.prefixCommands.enabled ||
+    !commands.prefixCommands.prefix ||
+    !message.content.startsWith(commands.prefixCommands.prefix)
+  )
+    return
+
+  const prefix = commands.prefixCommands.prefix
+  const content = message.content.slice(prefix.length)
+
+  if (commands.help.enabled && (content.startsWith('help') || commands.help.alias.includes(content))) {
+    await message.channel.sendTyping()
+    const arg = content.split(' ')
+    if (arg[1]) {
+      const commandsChoicesArray = []
+      for (const cmds in commands) {
+        if (commands[cmds].enabled && !['slashCommands', 'prefixCommands', 'language'].includes(cmds)) {
+          commandsChoicesArray.push(cmds)
+        }
+      }
+      if (commandsChoicesArray.includes(arg[1])) {
+        message.channel.send({ embeds: [await helpEmbed(client, arg[1])] })
+        return
+      }
+    }
+    message.channel.send({ embeds: [await helpEmbed(client)] })
+  }
+  if (commands.motd.enabled && (content === 'motd' || commands.motd.alias.includes(content))) {
+    await message.channel.sendTyping()
+    message.channel.send({ embeds: [await motdEmbed()] })
+  }
+  if (commands.ip.enabled && (content === 'ip' || commands.ip.alias.includes(content))) {
+    message.channel.send({ embeds: [ipEmbed] })
+  }
+  if (commands.site.enabled && mcserver.site && (content === 'site' || commands.site.alias.includes(content))) {
+    message.channel.send({ embeds: [siteEmbed] })
+  }
+  if (commands.version.enabled && (content === 'version' || commands.version.alias.includes(content))) {
+    message.channel.send({ embeds: [versionEmbed] })
+  }
+
+  if (commands.players.enabled && (content === 'players' || commands.players.alias.includes(content))) {
+    await message.channel.sendTyping()
+    message.channel.send({ embeds: [await playerList()] })
+  }
+  if (commands.status.enabled && (content === 'status' || commands.status.alias.includes(content))) {
+    await message.channel.sendTyping()
+    try {
+      message.channel.send({ embeds: [await statusEmbed()] })
+    } catch (error) {
+      message.channel.send(cmdSlashTranslation.status.errorReply)
+      const { getError } = require('../../index')
+      getError(error, 'statusPrefix')
+    }
+  }
+}
